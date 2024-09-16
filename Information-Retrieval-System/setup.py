7d1f4b359fd8e0e2235f4302a9284095e2d4d08a
pranay360 <mamullapranay360@gmail.com>
2024-09-16 06:11:50 +0530

    first

--- Information-Retrieval-System/setup.py
+++ Information-Retrieval-System/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'pranay',
+    author_email= 'bt21cse079@iiitn.ac.in',
     packages= find_packages(),
     install_requires = []
 
